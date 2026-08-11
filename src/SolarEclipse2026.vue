adda889c01d3b16cf74bf1305a631089aff1ffa9
John Arban Lewis <johnarban@users.noreply.github.com>
2026-08-11 10:38:57 -0400

    use UTC for hide rating time and place well after final partial eclipse. practically not important for real viewers though

--- src/SolarEclipse2026.vue
+++ src/SolarEclipse2026.vue
@@ -1686,9 +1686,9 @@ const DONT_SHOW_INTRO_KEY = "eclipse-2026-mini-dontshowintro" as const;
 
 /* Temporarily suppress the user experience rating and consequently
  * its privacy policy, which can only be reached from the "What is this" button
- * Will re-enables  automatically at **local** midnight on 2026-08-13.
+ * Will re-enables  automatically at **UTC** noon on 2026-08-13.
 */ 
-const HIDE_RATING_UNTIL = new Date(2026, 7, 13); // Aug 13, 2026, local time
+const HIDE_RATING_UNTIL = new Date(Date.UTC(2026, 7, 13, 12, 0)); // Aug 13, 2026, noon UTC, well after final partial
 
 
 const RELEVANT_FEATURE_TYPES = ["postcode", "place", "region", "country"];
